3ce66ce871be13214acdf81dd50f58dab91a0606
brunomdr <brunomdr46@gmail.com>
2022-07-29 11:43:57 -0300

    Adicionado estilização ao header

--- Desafio01 - Blog/src/components/Header/Header.tsx
+++ Desafio01 - Blog/src/components/Header/Header.tsx
@@ -3,8 +3,8 @@ import './styles.scss';
 function Header() {
   return (
     <div className="header-background">
-      <h1 className="title">Codelândia</h1>
-      <h2 className="subtitle">blog</h2>
+      <h1 className="header-text">Codelândia</h1>
+      <h2 className="header-text">blog</h2>
     </div>
   );
 }
