b753e49d7ebf1598b2443c2c978003800e57d6a0
brunomdr <brunomdr46@gmail.com>
2022-07-29 11:32:40 -0300

    Adicionado componente do Header

---
+++ Desafio01 - Blog/src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import './styles.scss';
+
+function Header() {
+  return (
+    <div className="header-background">
+      <h1 className="title">Codelândia</h1>
+      <h2 className="subtitle">blog</h2>
+    </div>
+  );
+}
+
+export default Header;
