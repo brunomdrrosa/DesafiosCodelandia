79a5e22cef8da6e2a9a86d3c2e9f8eadf778b4e9
brunomdr <brunomdr46@gmail.com>
2022-07-29 22:28:02 -0300

    Adicionado ícones clicáveis

--- Desafio01 - Blog/src/components/News/News.tsx
+++ Desafio01 - Blog/src/components/News/News.tsx
@@ -1,19 +1,28 @@
 import './styles.scss';
 import { FaRegHeart } from 'react-icons/fa';
+import { FaHeart } from 'react-icons/fa';
+import { useState } from 'react';
 
-interface NewsProps {
-  date: string;
-  title: string;
-  description: string;
+export interface NewsProps {
+  id?: number;
+  date?: string;
+  title?: string;
+  description?: string;
 }
 
 function News(props: NewsProps) {
+  const [icon, setIcon] = useState(true);
+
   return (
     <div className="background">
       <main className="news-box">
         <div className="row-date-heart">
           <h3 className="news-text date">{props.date}</h3>
-          <FaRegHeart color="#574AE8" size={25} />
+          {icon ? (
+            <FaRegHeart color="#574AE8" size={25} className="icon-heart" onClick={() => setIcon(!icon)} />
+          ) : (
+            <FaHeart color="#574AE8" size={25} className="icon-heart" onClick={() => setIcon(!icon)} />
+          )}
         </div>
         <h4 className="news-text news-title">{props.title}</h4>
         <p className="news-text description">{props.description}</p>
