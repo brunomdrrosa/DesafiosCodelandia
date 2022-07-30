666fb700db9948128ee6996e566e4d1c8e8ee105
brunomdr <brunomdr46@gmail.com>
2022-07-29 22:24:31 -0300

    Adicionado tipagem no array de notícias

--- Desafio01 - Blog/news.ts
+++ Desafio01 - Blog/news.ts
@@ -1,4 +1,6 @@
-const news = [
+import { NewsProps } from '../Desafio01 - Blog/src/components/News/News';
+
+const news: NewsProps[] = [
   {
     id: 1,
     date: '02 de jul, 2021',
@@ -10,19 +12,22 @@ const news = [
     id: 2,
     date: '02 de jul, 2021',
     title: 'Tem Firefox novo no pedaço e você vai querer migrar',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum auctor est.',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum auctor est.',
   },
   {
     id: 3,
     date: '02 de jul, 2021',
     title: 'Tim Berners-Lee vai leiloar código-fonte da web',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum auctor est. Nam vitae finibus ante. Duis lobortis tellus vel diam fringilla, eu ullamcorper ex iaculis. Praesent et auctor justo. Vestibulum nisl orci, lacinia venenatis leo sit amet, pulvinar tincidunt risus. Phasellus nisl dui, cursus a lectus et, interdum ullamcorper libero.',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum auctor est. Nam vitae finibus ante. Duis lobortis tellus vel diam fringilla, eu ullamcorper ex iaculis. Praesent et auctor justo. Vestibulum nisl orci, lacinia venenatis leo sit amet, pulvinar tincidunt risus. Phasellus nisl dui, cursus a lectus et, interdum ullamcorper libero.',
   },
   {
     id: 4,
     date: '02 de jul, 2021',
     title: 'Agora é oficial: o Windows 11 está vindo',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum auctor est. Nam vitae finibus ante. Duis lobortis tellus vel diam fringilla, eu ullamcorper ex iaculis.',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum auctor est. Nam vitae finibus ante. Duis lobortis tellus vel diam fringilla, eu ullamcorper ex iaculis.',
   },
 ];
 
